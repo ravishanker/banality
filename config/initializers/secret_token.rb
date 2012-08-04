acd5100b324ebce1b5f2c436c7da0db9edddbc45
Ravi Shanker <ravi.m.shanker@gmail.com>
2012-08-04 15:59:16 -0700

    Initialized banality app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Banality::Application.config.secret_token = 'cf128198ea0932b5dc437dbb2818ca4e3f40d1bbafebf0a70f2d885ee0d57fb37d99474f7b4eea295317483fc7dcabe0cc582e8ad102d5ce8d7ab2030f9eb0fd'
